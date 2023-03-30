362146fb0f8f998fb424624085f8523c03880fe1
Alessio Boscaro <119036817+alessiobo@users.noreply.github.com>
2023-03-30 11:44:42 +0200

    add mapped

--- exercise-1-primitive-types-start.ts
+++ exercise-1-primitive-types-start.ts
@@ -2,50 +2,26 @@
  * START: Follow the instructions below.
  */
 
-interface Country {
+type CountryData = {
     name: string;
-    code: string;
-}
-
-interface CountryStatistics {
-    population: number;
-}
-
-interface CountryLanguages {
     languages: string[];
-}
-
-// Change the `CountryWithLanguages` type into an intersection type
-// that uses the interfaces defined above.
-//
-// This should fix the type error on Line 28.
-
-type CountryWithLanguages = Country & CountryLanguages;
-
-const countryA: CountryWithLanguages = {
-    name: "Greece",
-    code: "GR",
-    languages: ["Greek"]
-};
-
-// Create an intersection type named `CountryWithStatistics` that
-// describes the shape of the object below. Use the interfaces
-// defined above.
-//
-// Use the type alias syntax: type CountryWithStatistics = type;
-//
-// Add a type annotation with the `CountryWithStatistics` type on
-// the variable `countryB`.
-
-type CountryWithStatistics = Country & {
-    code: string;
     population: number;
 };
 
-const countryB: CountryWithStatistics = {
-    name: "China",
-    code: "CN",
-    population: 1_412_600_000
+// Complete the syntax for this mapped type.
+// Hint: Mapped types are generic.
+//
+// Replace the `never` type with the correct type.
+//
+// These changes should fix all the type errors.
+
+type Descriptions<Type> = {
+    [Property in keyof Type]: string;
+};
+const countryDataDescriptions: Descriptions<CountryData> = {
+    name: "The name of the country.",
+    languages: "The languages spoken in the country.",
+    population: "The population of the country.",
 };
 
 // ----
